2bfa475566a95944a481aa2ac97e2f1b0c877e68
Caio Cesar <caio.sfg@gmail.com>
2021-04-18 00:04:30 -0300

    Add controllers

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ new Vue({
     el:'#app',
     data:{
         kongLife : 100,
-        godzillaLife : 100
+        godzillaLife : 100,
+        running : false
     },
     computed:{
-
+        hasResult() {
+            return this.kongLife == 0 || this.godzillaLife == 0
+        }
     },
     methods:{
+        startGame() {
+            this.running = true
+            this.kongLife = 100
+            this.godzillaLife = 100
+
+        }
 
     },
     watch:{
