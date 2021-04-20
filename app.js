6532a731fe39f3166c03077cab5e3cdcd3ba325a
Caio Cesar <caio.sfg@gmail.com>
2021-04-20 00:14:34 -0300

    algumas insercoes

--- app.js
+++ app.js
@@ -15,11 +15,26 @@ new Vue({
             this.running = true
             this.kongLife = 100
             this.godzillaLife = 100
+        },
+        attack(special) {
+            this.hurt('kongLife',7, 12, false)
+            this.hurt('godzillaLife',5, 10, special)
+        },
+        hurt(atr, min, max, special) {
+            const plus = special ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[atr] = Math.max(this[atr] - hurt, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * ( max - min ) + min
 
+            return Math.round(value)
         }
 
     },
     watch:{
-
+        hasResult(value) {
+            if (value) this.running = false
+        }
     }
 })
